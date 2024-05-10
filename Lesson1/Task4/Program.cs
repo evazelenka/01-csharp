01320a65cbd4d6201ed015035462ca125a1f5b2b
zelenka <eva.zelya@gmail.com>
2024-05-10 17:12:29 +0300

    add lesson1:task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 5;
+int b = 8;
+int c = 15;
+int d = 7;
+int e = 3;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
