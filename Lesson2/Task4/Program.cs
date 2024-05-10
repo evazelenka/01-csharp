cd4d13a5c1f51856011c43bb8a52f765ae2fd83c
zelenka <eva.zelya@gmail.com>
2024-05-10 20:40:59 +0300

    add lesson2:task4

---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = 5;
+int[] arr = {1,2,5,7,8};
+int i = 0;
+int max = arr[0];
+
+while(i < n)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i = i + 1;
+}
+Console.Write(max);
