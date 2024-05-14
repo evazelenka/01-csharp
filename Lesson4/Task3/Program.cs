97a27cd076db6519387d9db920c8f107f884d900
zelenka <eva.zelya@gmail.com>
2024-05-14 22:33:12 +0300

    add lesson4:task3

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿string GetLetters(string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if(char.IsLetter(e) == true)
+        {
+            letters += e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLetters(str);
+Console.WriteLine(result);
